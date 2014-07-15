0939f98dd52a7ae33f66a75a6c782c6fc40da5f8
bastian153 <ruben@Rubens-MacBook-Pro-2.local>
2014-07-15 09:58:48 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Ruben on 7/15/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
